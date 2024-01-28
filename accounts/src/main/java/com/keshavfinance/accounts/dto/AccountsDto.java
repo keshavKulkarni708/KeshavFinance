8b6c86a9ed3e0f78284e94021d5b404ea77dec82
Keshav <keshav.kulkarni708>
2024-01-28 14:30:41 +0530

    Added input data validations inside accounts microservice

--- accounts/src/main/java/com/keshavfinance/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/keshavfinance/accounts/dto/AccountsDto.java
@@ -2,14 +2,19 @@ package com.keshavfinance.accounts.dto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account type cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch address cannot be null or empty")
     private String branchAddress;
 }
